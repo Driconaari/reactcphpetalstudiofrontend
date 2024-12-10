a36d9aaf7fbeb7a6536bcd1fb8e7393e10db339b
asger <asgervb@gmail.com>
2024-12-11 00:29:31 +0100

    added router.tsx

---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ShopPage from './pages/ShopPage';
+import CartPage from './pages/CartPage';
+import UserProfilePage from './pages/UserProfilePage';
+
+const RouterConfig = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/shop" element={<ShopPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/profile" element={<UserProfilePage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default RouterConfig;
